53fd549e42d2413e3f67c7870ba07a4c8e76be46
sato0414 <sato@fintechsys.co.jp>
2025-01-20 21:03:52 +0900

    iROASOBi_03

--- js/script.js
+++ js/script.js
@@ -48,3 +48,25 @@ $(document).ready(function() {
         }
     });
 });
+
+//ハンバーガーメニュー
+(function($) {
+    var $nav   = $('#navArea');
+    var $btn   = $('.toggle_btn');
+    var $mask  = $('#mask');
+    var open   = 'open'; // class
+
+    // ハンバーガーメニューの開閉
+    $btn.on( 'click', function() {
+      if ( ! $nav.hasClass( open ) ) {
+        $nav.addClass( open );
+      } else {
+        $nav.removeClass( open );
+      }
+    });
+
+    // マスクをクリックして閉じる
+    $mask.on('click', function() {
+      $nav.removeClass( open );
+    });
+})(jQuery);
