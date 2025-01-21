adcc43c033ca09aca338196e5205c8f21a9efa9f
sato0414 <sato@fintechsys.co.jp>
2025-01-21 18:59:07 +0900

    iROASOBi_04

--- js/script.js
+++ js/script.js
@@ -70,3 +70,38 @@ $(document).ready(function() {
       $nav.removeClass( open );
     });
 })(jQuery);
+
+
+$(function () {
+    // 変数に要素をセット
+    var tabList = $('.works_bar li'),
+        allWorks = $('.works_wrapper.all'),
+        lpWorks = $('.works_wrapper.lp'),
+        webWorks = $('.works_wrapper.web'),
+        bannerWorks = $('.works_wrapper.banner');
+    
+    // 最初の要素のみを表示させる
+    $('.works_wrapper').hide();  // すべて隠す
+    allWorks.show();  // ALLを最初に表示
+    
+    // タブをクリックしたら、対応する要素を表示
+    tabList.click(function () {
+        tabList.removeClass('active');
+        $(this).addClass('active');
+        
+        var index = tabList.index(this);
+        
+        // すべて隠してから該当するものを表示
+        $('.works_wrapper').hide();
+        
+        if (index === 0) {
+            allWorks.show(); // ALLを表示
+        } else if (index === 1) {
+            lpWorks.show(); // LPを表示
+        } else if (index === 2) {
+            webWorks.show(); // WEBサイトを表示
+        } else if (index === 3) {
+            bannerWorks.show(); // バナーを表示
+        }
+    });
+});
